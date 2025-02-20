b6648180e03f5e53198225474a4c364b2f0fb2f6
Ian <neocorpse@proton.me>
2025-02-20 22:22:35 +0300

    Complete lab by using relevant loops

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length ; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+
+    return gifts
+}
+
+wrapGifts(gifts)
+
+function writeCards(arr, e) {
+    let array = []
+    for(let i = 0; i < arr.length; i++) {
+        array.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+    }
+
+    return array
+}
+
+function countDown(num) {
+    while(num >= 0) {
+        debugger;
+        console.log(num--)
+    }
+}
